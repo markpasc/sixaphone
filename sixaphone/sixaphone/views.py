4b8bdcd234ffaa1b5493e51e2a78bf2d585ecad0
Mark Paschal <markpasc@markpasc.org>
2010-02-03 23:45:16 -0800

    Add 'Play all' button

--- sixaphone/sixaphone/views.py
+++ sixaphone/sixaphone/views.py
@@ -5,6 +5,18 @@ from templateresponse import TemplateResponse
 import typepad
 
 
+def audio_events_from_events(events):
+    for event in events:
+        obj = event.object
+        if obj is None:
+            continue
+        if obj.object_type != 'tag:api.typepad.com,2009:Audio':
+            continue
+        for audio in obj.links['rel__enclosure']:
+            yield audio, event
+            break  # only need the first
+
+
 def home(request):
     with typepad.client.batch_request():
         request.user = get_user(request)
@@ -12,4 +24,5 @@ def home(request):
 
     return TemplateResponse(request, 'sixaphone/home.html', {
         'events': events,
+        'audio_events': audio_events_from_events(events),
     })
