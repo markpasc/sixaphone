cfe2baacc880424d7c937fa5700d1ee1fb2bdaef
Mark Paschal <markpasc@markpasc.org>
2010-02-09 16:51:55 -0800

    Hook up the favorites button

--- sixaphone/sixaphone/urls.py
+++ sixaphone/sixaphone/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     url(r'^$', 'sixaphone.views.home', name='home'),
     url(r'^entry/(?P<xid>6a[^/]+)$', 'sixaphone.views.entry', name='entry'),
 
+    url(r'^favorite$', 'sixaphone.views.favorite', name='favorite'),
     url(r'^new_post$', 'sixaphone.views.new_post'),
 )
--- sixaphone/sixaphone/views.py
+++ sixaphone/sixaphone/views.py
@@ -1,11 +1,30 @@
 from __future__ import with_statement
 
+import functools
+import logging
+import re
+
 from django.contrib.auth import get_user
 from django.http import HttpResponse
 from templateresponse import TemplateResponse
 import typepad
 from typepadapp.caching import CacheInvalidator
-from typepadapp.models import Asset
+from typepadapp.models import Asset, Favorite
+import typepadapp.signals
+
+
+log = logging.getLogger(__name__)
+
+
+def oops(fn):
+    @functools.wraps(fn)
+    def hoops(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except Exception, exc:
+            log.exception(exc)
+            return HttpResponse('%s: %s' % (type(exc).__name__, str(exc)), status=400, content_type='text/plain')
+    return hoops
 
 
 def audio_events_from_events(events):
@@ -50,6 +69,33 @@ def entry(request, xid):
     })
 
 
+@oops
+def favorite(request):
+    if request.method != 'POST':
+        return HttpResponse('POST required at this url', status=400, content_type='text/plain')
+
+    action = request.POST.get('action', 'favorite')
+    asset_id = request.POST.get('asset_id', '')
+    try:
+        (asset_id,) = re.findall('6a\w+', asset_id)
+    except TypeError:
+        raise Http404
+
+    if action != 'favorite':
+        return HttpResponse('Unsupported action %r' % action, status=400, content_type='text/plain')
+
+    with typepad.client.batch_request():
+        asset = Asset.get_by_url_id(asset_id)
+    fav = Favorite()
+    fav.in_reply_to = asset.asset_ref
+    request.user.favorites.post(fav)
+    typepadapp.signals.favorite_created.send(sender=fav, instance=fav, parent=asset,
+        group=request.group)
+
+    return HttpResponse('OK', content_type='text/plain')
+
+
+@oops
 def new_post(request):
     if request.method != 'POST':
         return HttpResponse('POST required', status=400, content_type='text/plain')
