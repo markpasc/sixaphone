05154d03d692f8b2822b4a3ef73aebd11abefc78
Mark Paschal <markpasc@markpasc.org>
2010-02-19 18:09:21 -0800

    Oops, asset XIDs are wider than that

--- sixaphone/sixaphone/models.py
+++ sixaphone/sixaphone/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Tag(models.Model):
     tag = models.CharField(max_length=50)  # tag text
-    asset = models.CharField(max_length=30)  # xid
+    asset = models.CharField(max_length=36)  # xid
 
     by = models.CharField(max_length=30)  # xid
     by_name = models.CharField(max_length=50)  # display name
